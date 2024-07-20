89c6acf32059d9d42f2e1113557f4398773e874b
dubininme <dubinin.me@gmail.com>
2024-07-20 11:24:27 +0200

    simplified and improved solution

---
+++ contains_duplicate/contains_duplicate_2_imp.go
@@ -0,0 +1,15 @@
+package contains_duplicate
+
+func containsNearbyDuplicate2Imp(nums []int, k int) bool {
+	dict := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		lastIdx, exists := dict[nums[i]]
+		if exists && (i-lastIdx) <= k {
+			return true
+		}
+
+		dict[nums[i]] = i
+	}
+
+	return false
+}
